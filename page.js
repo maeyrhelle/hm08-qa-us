9ce502bfc00ea0867bfe50edbe96b5a3af53985e
maeyrhelle <144966644+maeyrhelle@users.noreply.github.com>
2024-05-18 15:53:40 -0400

    Update page.js

--- page.js
+++ page.js
@@ -2,15 +2,29 @@ module.exports = {
     // Inputs
     fromField: '#from',
     toField: '#to',
-    phoneNumberField: '#phone',
+    phoneNumberField: '#phone', 
     codeField: '#code',
+    cvvField: '.card-code-input #code', 
+    cardNumberField: '#number.card-input',
+    messageForDriverField: '#comment',
     // Buttons
     callATaxiButton: 'button=Call a taxi',
     phoneNumberButton: '//div[starts-with(text(), "Phone number")]',
     nextButton: 'button=Next',
     confirmButton: 'button=Confirm',
+    supportivePlanButton: 'div=Supportive',
+    paymentMethodButton: '.pp-text',
+    addCardButton: 'div=Add card',
+    linkButton: 'button=Link',
+    blanketAndHandkerchiefsButton: '.r-sw',
+    blanketSwitch: '.switch-input',
+    addIcecreamButton: '.counter-plus',
+    orderButton: '.smart-button',
     // Modals
     phoneNumberModal: '.modal',
+    paymentMethodModal: '.payment-picker .modal',
+    carSearchModal: '.order-body',
+    driverInfo: '.order-header-title',
     // Functions
     fillAddresses: async function(from, to) {
         const fromField = await $(this.fromField);
@@ -26,7 +40,7 @@ module.exports = {
         await phoneNumberButton.waitForDisplayed();
         await phoneNumberButton.click();
         const phoneNumberModal = await $(this.phoneNumberModal);
-        await phoneNumberModal.waitForDisplayed()
+        await phoneNumberModal.waitForDisplayed();
         const phoneNumberField = await $(this.phoneNumberField);
         await phoneNumberField.waitForDisplayed();
         await phoneNumberField.setValue(phoneNumber);
@@ -43,9 +57,25 @@ module.exports = {
         // collect all responses
         const requests = await browser.getRequests();
         // use first response
-        await expect(requests.length).toBe(1)
-        const code = await requests[0].response.body.code
-        await codeField.setValue(code)
-        await $(this.confirmButton).click()
+        await expect(requests.length).toBe(1);
+        const code = await requests[0].response.body.code;
+        await codeField.setValue(code);
+        await $(this.confirmButton).click();
     },
+    fillCardNumber: async function(cardNumber) {
+        const paymentMethodButton = await $(this.paymentMethodButton);
+        await paymentMethodButton.waitForDisplayed();
+        await paymentMethodButton.click();
+        const addCardButton = await $(this.addCardButton);
+        await addCardButton.waitForDisplayed();
+        await addCardButton.click();
+        const cardNumberField = await $(this.cardNumberField);
+        await cardNumberField.setValue(cardNumber);
+    },
+    fillCvvCode: async function(codeNumber) {
+        const cvvField = await $(this.cvvField);
+        await cvvField.setValue(codeNumber);
+        const linkButton = await  $(this.linkButton);
+        await linkButton.click();
+    }
 };
